811fdc2aa0887979fc03bc94516ad45edf0857fc
Tsavoka <aenore@ya.ru>
2025-02-06 16:56:24 +0300

    new requirements and new marks for testing

--- test_product_page.py
+++ test_product_page.py
@@ -3,10 +3,10 @@ from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 import pytest
 
-'''
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6",
                                          pytest.param("7", marks=pytest.mark.xfail), 
                                          "8", "9"])
@@ -41,14 +41,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.success_message_should_disappear()
-'''
+
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-'''
+
 def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -56,13 +57,14 @@ def test_guest_can_go_to_login_page(browser):
     #login_page = LoginPage(browser, browser.current_url)
     #login_page.should_be_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
-'''
+
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -82,6 +84,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
